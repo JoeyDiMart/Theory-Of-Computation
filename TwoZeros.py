3b1761deb0f67b2650edb767c6e8b45531ce1e66
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2026-01-27 20:54:52 -0500

    Update TwoZeros.py

--- TwoZeros.py
+++ TwoZeros.py
@@ -2,18 +2,16 @@
 # all strings within the alphabet {0,1} and contains at least two zeros
 
 
-def isAccepted(s, i=0, c=0):
+def isAccepted(s, c=0):
     if s[0] == "0" and c == 0:
-        i += 1
         c = 1
-        isAccepted(s[i:], i, c)
+        return isAccepted(s[1:], c)
     elif s[0] == "0" and c == 1:
         return "Accepted"
+    elif len(s) == 1:
+        return "Not Accepted"
     else:
-        if s[-1] == s[0]:
-            return "Not Accepted"
-    i += 1
-    isAccepted(s[i:], i, c)
+        return isAccepted(s[1:], c)
 
 
 s = input("enter a string of 1's and 0's: ")
