ad25ecdb6917517997e6978848eade1dd3c37b56
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2026-02-19 17:18:04 -0500

    new extra credit program

--- AtLeastFiveOnes.py
+++ AtLeastFiveOnes.py
@@ -1,6 +1,1 @@
 # prove a DFA that contains at least five 1's
-
-
-
-
-s = 
---
+++ ProveSubset1.py
@@ -0,0 +1,4 @@
+'''
+Program: Let L be the regular language of all strings ending in a 1 and M be the regular language
+of all strings that both start and end with a 1. Prove M is a subset of L by writing a program.
+'''
